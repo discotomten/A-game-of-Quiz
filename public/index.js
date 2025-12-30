c571ff19491fb0648b0b70276c5863f40558e42a
discotomten <linkaan_lonn@hotmail.com>
2025-12-30 13:47:53 +0100

    connecting buttons to variables

---
+++ public/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const startMenu = document.getElementById("startMenu");
+const categorySelect = document.getElementById("categorySelect");
+const startBtn = document.getElementById("startBtn");
+const settingsBtn = document.getElementById("settingsBtn"); //TODO ändra färger i menyn?
+const settingScreen = document.getElementById("settingScreen");
+const quizScreen = document.getElementById("quizScreen");
+const questionDiv = document.getElementById("question");
+const nextBtn = document.getElementById("nextBtn");
+const optionsDiv = document.getElementById("options");
+const backBtn = document.getElementById("backBtn");
