2dd80bad4be37aa67eb8c0ac03a160b80c46db4e
discotomten <linkaan_lonn@hotmail.com>
2025-12-30 13:16:21 +0100

    adding a GET for my questions

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//category är en platshållare som matchar vad som helst i den positionen t.ex. animals
+app.get("/api/quiz/:category", (req, res) => {
+  const { category } = req.params;
+  const getQuestions = questions[category];
+
+  if (!getQuestions) {
+    return res.status(404).json({ error: "Category not found" });
+  }
+  res.json(getQuestions);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
