f1c1b76d3e61d5548dc1288340e9533b2bd0309a
discotomten <linkaan_lonn@hotmail.com>
2026-01-01 16:00:24 +0100

    switching between themes on click

--- public/settings.js
+++ public/settings.js
@@ -2,3 +2,12 @@ export function setTheme(theme) {
   document.documentElement.style.setProperty("--bg", theme.bg);
   document.documentElement.style.setProperty("--text", theme.text);
 }
+export function saveTheme() {
+  const themeColor = document.querySelectorAll(".colorBtn");
+  themeColor.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const theme = JSON.parse(btn.dataset.theme);
+      setTheme(theme);
+    });
+  });
+}
