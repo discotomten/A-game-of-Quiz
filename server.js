38b1dfd9ba9df4eb42b59d88a0a2ec33d8f69cb8
discotomten <linkaan_lonn@hotmail.com>
2026-01-01 22:09:50 +0100

    using url to find an absolute path

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 "use strict";
 import express from "express";
 import questions from "./server/data/questions.js";
+import path from "path"; // path är ett inbyggt node-paket, används för att bygga paths säkert
+import { fileURLToPath } from "url";
 
 const port = 80;
 
+const fileName = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
 const app = express();
 app.use(express.json());
 
