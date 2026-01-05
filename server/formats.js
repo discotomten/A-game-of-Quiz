3da5f53f1193e2d512aa21d6808f610bda1fbfc1
discotomten <linkaan_lonn@hotmail.com>
2026-01-05 23:06:32 +0100

    adding a hash to the password

--- server/formats.js
+++ server/formats.js
@@ -5,3 +5,11 @@ export function isValidEmail(email) {
   return mailcheck.test(email);
 }
 
+export function simpleHash(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i);
+    hash |= 0;
+  }
+  return hash.toString(16);
+}
