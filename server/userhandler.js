779a8d3937c7da5b1e7f43baa5f0c12c71116917
discotomten <linkaan_lonn@hotmail.com>
2026-01-05 22:15:23 +0100

    function for writing a new user to the jsonFile

--- server/userhandler.js
+++ server/userhandler.js
@@ -6,3 +6,10 @@ export async function getUsers() {
   const data = await fs.readFile(filePath, "utf-8");
   return JSON.parse(data);
 }
+
+export async function saveUsers(users) {
+  await fs.writeFile(
+    filePath,
+    JSON.stringify(users, null, 2) // null betyder ändra ingenting och 2 gör två mellanslag per nivå, lättare att läsa i json
+  );
+}
